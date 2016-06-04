b14b392eec285899d6dbe87e6c1fe4981a0c3f8d
david0932 <david0932@yahoo.com.tw>
2016-06-04 14:00:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	CHANNEL_ID: ' ',
-	CHANNEL_SERECT: ' ',
-	MID: ' '
+	CHANNEL_ID: '1461915747',
+	CHANNEL_SERECT: 'bcd16b63fec39f9043a1e4cdd6cfc1f0',
+	MID: 'u0e0d4c3578e381f4ce7cce8804eeb8a3'
 }
